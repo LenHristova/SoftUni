dbcfce0b5e75bb503a0416f070c654e4ca6ce372
LenHristova <len_5@abv.bg>
2017-09-30 00:11:49 +0300

    Modify p02_CircleArea

---
+++ TechModule/ProgramingFundamentals/DataTypesAndVariablesLab/p02_CircleArea/CircleArea.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace p02_CircleArea
+{
+    class CircleArea
+    {
+        static void Main(string[] args)
+        {
+            double radius = double.Parse(Console.ReadLine());
+
+            double circleArea = Math.PI * radius * radius;
+            Console.WriteLine($"{circleArea:f12}");
+        }
+    }
+}
