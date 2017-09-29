0fb2f327b46627d49648511f1834a1cc832feecd
LenHristova <len_5@abv.bg>
2017-09-30 00:06:33 +0300

    Modify CenturiesToMinutes.cs

--- TechModule/ProgramingFundamentals/DataTypesAndVariablesLab/p01_CenturiesToMinutes/CenturiesToMinutes.cs
+++ TechModule/ProgramingFundamentals/DataTypesAndVariablesLab/p01_CenturiesToMinutes/CenturiesToMinutes.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 
 namespace p01_CenturiesToMinutes
 {
-    class Program
+    class CenturiesToMinutes
     {
         static void Main(string[] args)
         {
+            byte centuries = byte.Parse(Console.ReadLine());
+            ushort years = (ushort)(centuries * 100);
+            int days = (int)(years * 365.2422);
+            uint hours = (uint)(days * 24);
+            long minutes = hours * 60;
+
+            Console.WriteLine($"{centuries} centuries = " +
+                $"{years} years = {days} days = " +
+                $"{hours} hours = {minutes} minutes");
         }
     }
 }
