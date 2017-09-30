81a4d03957f640f785d5f0625e1b69955d125025
LenHristova <len_5@abv.bg>
2017-09-30 11:44:02 +0300

    Add p03_ExactSumOfRealNumbers

---
+++ TechModule/ProgramingFundamentals/DataTypesAndVariablesLab/p03_ExactSumOfRealNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace p03_ExactSumOfRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numsCount = int.Parse(Console.ReadLine());
+            decimal exactSum = 0M;
+            for (int currentNum = 0; currentNum < numsCount; currentNum++)
+            {
+                exactSum += decimal.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(exactSum);
+        }
+    }
+}
