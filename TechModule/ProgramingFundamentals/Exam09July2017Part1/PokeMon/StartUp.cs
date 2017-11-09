1e6fc283810d33e31494f23616bd055e34b56738
LenHristova <len_5@abv.bg>
2017-11-09 17:56:44 +0200

    Add Exam09July2017Part1.sln

---
+++ TechModule/ProgramingFundamentals/Exam09July2017Part1/PokeMon/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PokeMon
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            int targetsCount = 0;
+            int rem = n;
+            while (rem >= m)
+            {
+                rem -= m;
+                targetsCount++;
+                if (rem < m)
+                {
+                    break;
+                }
+                if (rem == (double)n / 2 && y != 0)
+                {
+                    rem /= y;
+                }
+            }
+
+            Console.WriteLine(rem);
+            Console.WriteLine(targetsCount);
+        }
+    }
+}
---
+++ TechModule/ProgramingFundamentals/Exam09July2017Part1/PokemonDontGo/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PokemonDontGo
+{
+    class StartUp
+    {
+        static void Main()
+        {
+            List<int> distances = Console.ReadLine()
+                .Split(new[] { ' ', '\t', '\n' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            long sum = 0L;
+            while (distances.Count > 0)
+            {
+                int index = int.Parse(Console.ReadLine());
+
+                int num;
+                if (index < 0)
+                {
+                    num = distances.First();
+                    distances.RemoveAt(0);
+                    distances.Insert(0, distances.Last());
+                }
+                else if (index > distances.Count - 1)
+                {
+                    num = distances.Last();
+                    distances.RemoveAt(distances.Count - 1);
+                    distances.Add(distances.First());
+                }
+                else
+                {
+                    num = distances[index];
+                    distances.RemoveAt(index);
+                }
+
+                sum += num;
+
+                distances = distances
+                    .Select(n => n <= num ? n + num : n - num)
+                    .ToList();
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
