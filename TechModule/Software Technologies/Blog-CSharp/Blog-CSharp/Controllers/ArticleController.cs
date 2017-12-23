595d455ce73994a698e1cbc48023d8dcfb757fca
LenHristova <len_5@abv.bg>
2017-12-24 00:27:27 +0200

    Add Blog-CSharp

---
+++ TechModule/Software Technologies/Blog-CSharp/Blog-CSharp/Controllers/ArticleController.cs
@@ -0,0 +1,218 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using Blog_CSharp.Models;
+
+namespace Blog_CSharp.Controllers
+{
+    public class ArticleController : Controller
+    {
+        // GET: Article
+        public ActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        // GET: Article/List
+        public ActionResult List()
+        {
+            using (var database = new BlogDbContext())
+            {
+                var articles = database.Articles
+                    .Include(a => a.Author)
+                    .ToList();
+
+                return View(articles);
+            }            
+        }
+
+        //GET: Article/Details
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (var database = new BlogDbContext())
+            {
+                var article = database.Articles
+                    .Where(a => a.Id == id)
+                    .Include(a => a.Author)
+                    .FirstOrDefault();
+
+                if (article == null)
+                {
+                    return HttpNotFound();
+                }
+
+                return View(article);
+            }
+        }
+
+        //GET: Article/Create
+        [Authorize]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //POST: Article/Create
+        [HttpPost]
+        [Authorize]
+        public ActionResult Create(Article article)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var database = new BlogDbContext())
+                {
+                    var authorId = database.Users
+                        .First(u => u.UserName == User.Identity.Name)
+                        .Id;
+
+                    article.AuthorId = authorId;
+
+                    database.Articles.Add(article);
+                    database.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return View(article);
+        }
+
+        //GET: Article/Delete
+        [Authorize]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (var database = new BlogDbContext())
+            {
+                var article = database.Articles
+                    .Where(a => a.Id == id)
+                    .Include(a => a.Author)
+                    .FirstOrDefault();
+
+                if (article == null)
+                {
+                    return HttpNotFound();
+                }
+
+                if (!IsUserAuthorizedToEdit(article))
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+                }
+
+                return View(article);
+            }
+        }
+
+        //POST: Article/Delete
+        [HttpPost]
+        [Authorize]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfermed(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (var database = new BlogDbContext())
+            {
+                var article = database.Articles
+                    .Where(a => a.Id == id)
+                    .Include(a => a.Author)
+                    .FirstOrDefault();
+
+                if (article == null)
+                {
+                    return HttpNotFound();
+                }
+
+
+                database.Articles.Remove(article);
+                database.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        //GET: Article/Edit
+        [Authorize]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (var database = new BlogDbContext())
+            {
+                var article = database.Articles
+                    .FirstOrDefault(a => a.Id == id);
+
+                if (article == null)
+                {
+                    return HttpNotFound();
+                }
+
+                if (!IsUserAuthorizedToEdit(article))
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.Forbidden);
+                }
+
+                var model = new ArticleViewModel
+                {
+                    Id = article.Id,
+                    Title = article.Title,
+                    Content = article.Content
+                };
+
+                return View(model);
+            }
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult Edit(ArticleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var database = new BlogDbContext())
+                {
+                    var article = database.Articles
+                        .FirstOrDefault(a => a.Id == model.Id);
+
+                    if (article == null)
+                    {
+                        return HttpNotFound();
+                    }
+
+                    article.Title = model.Title;
+                    article.Content = model.Content;
+
+                    database.Entry(article).State = EntityState.Modified;
+                    database.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return View(model);
+        }
+
+        private bool IsUserAuthorizedToEdit(Article article)
+        {
+            bool isAdmin = User.IsInRole("Admin");
+            bool isAuthor = article.IsAuthor(User.Identity.Name);
+
+            return isAdmin || isAuthor;
+        }
+    }
+}
---
+++ TechModule/Software Technologies/Blog-CSharp/Blog-CSharp/Startup.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Data.Entity;
+using Blog_CSharp.Migrations;
+using Blog_CSharp.Models;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Blog_CSharp.Startup))]
+namespace Blog_CSharp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            Database.SetInitializer(
+                new MigrateDatabaseToLatestVersion<BlogDbContext, Configuration>());
+            ConfigureAuth(app);
+        }
+    }
+}
