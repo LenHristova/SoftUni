23cc818c65f7508cc569c3409fdea94c4ad73add
LenHristova <len_5@abv.bg>
2017-12-24 03:23:14 +0200

    Add teistermask-Java

---
+++ TechModule/Software Technologies/TeisterMask-Java/src/main/java/teistermask/entity/Task.java
@@ -0,0 +1,48 @@
+package teistermask.entity;
+
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    @Length(min = 1)
+    private String title;
+
+    @Column(nullable = false)
+    @Length(min = 1)
+    private String status;
+
+    public Task() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
