2e6cac5f37005b852d67685023bbbeaaf2d14f08
LenHristova <len_5@abv.bg>
2018-01-17 02:07:28 +0200

    Add LinkedQueue - Implementation

---
+++ DataStructures/LinkedQueue/LinkedQueue/LinkedQueue.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+class LinkedQueue<T>
+{
+    private Node<T> _head;
+    private Node<T> _tail;
+
+    public int Count { get; set; }
+
+    public void Enqueue(T element)
+    {
+        var newNode = new Node<T>(element);
+
+        if (IsEmpty())
+        {
+            this._head = this._tail = newNode;
+        }
+        else
+        {
+            newNode.Prev = this._tail;
+            this._tail.Next = newNode;
+            this._tail = newNode;
+        }
+
+        this.Count++;
+    }
+
+    public T Dequeue()
+    {
+        if (IsEmpty())
+        {
+            throw new InvalidOperationException("LinkedQueue is empty.");
+        }
+
+        var element = this._head.Value;
+
+        if (this.Count == 1)
+        {
+            this._head = this._tail = null;
+        }
+        else
+        {
+            this._head = this._head.Next;
+            this._head.Prev = null;
+        }
+
+        this.Count--;
+        return element;
+    }
+
+    public T Peek()
+    {
+        if (IsEmpty())
+        {
+            throw new InvalidOperationException("LinkedQueue is empty!");
+        }
+
+        return this._head.Value;
+    }
+
+    public T[] ToArray()
+    {
+        var arr = new T[this.Count];
+        var current = this._head;
+        for (int i = 0; i < this.Count; i++)
+        {
+            arr[i] = current.Value;
+            current = current.Next;
+        }
+
+        return arr;
+    }
+
+    private bool IsEmpty()
+    {
+        return this.Count == 0;
+    }
+}
---
+++ DataStructures/LinkedQueue/LinkedQueue/Node.cs
@@ -0,0 +1,13 @@
+﻿class Node<T>
+{
+    public T Value { get; set; }
+
+    public Node<T> Next { get; set; }
+
+    public Node<T> Prev { get; set; }
+
+    public Node(T value)
+    {
+        Value = value;
+    }
+}
