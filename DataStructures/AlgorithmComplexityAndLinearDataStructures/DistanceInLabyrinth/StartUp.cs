2f54f33baa5390f025cf134a3f9bbdc09f9b95a8
LenHristova <len_5@abv.bg>
2018-01-17 14:45:54 +0200

    Modify DistanceInLabyrinth/StartUp.cs

--- DataStructures/AlgorithmComplexityAndLinearDataStructures/DistanceInLabyrinth/StartUp.cs
+++ DataStructures/AlgorithmComplexityAndLinearDataStructures/DistanceInLabyrinth/StartUp.cs
@@ -3,6 +3,20 @@ using System.Collections.Generic;
 
 class StartUp
 {
+    class Cell
+    {
+        public int Row { get; }
+        public int Col { get; }
+        public int Value { get; }
+
+        public Cell(int row, int col, int value)
+        {
+            this.Row = row;
+            this.Col = col;
+            this.Value = value;
+        }
+    }
+
     //private static string[,] labyrinth =
     //{
     //    { "0", "0", "0", "x", "0", "x" },
@@ -13,14 +27,14 @@ class StartUp
     //    { "0", "0", "0", "x", "0", "x" }
     //};
 
-    private static string[,] labyrinth;
+    private static string[,] _labyrinth;
+    private static Cell startingPosition;
 
     private static readonly Queue<Cell> VisitedCells = new Queue<Cell>();
 
     static void Main()
     {
-        labyrinth = GetLabyrinth();
-        var startingPosition = GetStartingPosition();
+        GetLabyrinthParams();
 
         VisitedCells.Enqueue(startingPosition);
 
@@ -34,88 +48,66 @@ class StartUp
             var value = currentCell.Value + 1;
 
             //Check available steps and mark them with their sequence
-            if (HasStepTo(row + 1, col))
+            if (row + 1 < _labyrinth.GetLongLength(0) && _labyrinth[row + 1, col] == "0")
             {
                 VisitedCells.Enqueue(new Cell(row + 1, col, value));
-                labyrinth[row + 1, col] = value.ToString();
+                _labyrinth[row + 1, col] = value.ToString();
             }
-            if (HasStepTo(row - 1, col))
+            if (row - 1 >= 0 && _labyrinth[row - 1, col] == "0")
             {
                 VisitedCells.Enqueue(new Cell(row - 1, col, value));
-                labyrinth[row - 1, col] = value.ToString();
+                _labyrinth[row - 1, col] = value.ToString();
             }
-            if (HasStepTo(row, col + 1))
+            if (col + 1 < _labyrinth.GetLongLength(1) && _labyrinth[row, col + 1] == "0")
             {
                 VisitedCells.Enqueue(new Cell(row, col + 1, value));
-                labyrinth[row, col + 1] = value.ToString();
+                _labyrinth[row, col + 1] = value.ToString();
             }
-            if (HasStepTo(row, col - 1))
+            if (col - 1 >= 0 && _labyrinth[row, col - 1] == "0")
             {
                 VisitedCells.Enqueue(new Cell(row, col - 1, value));
-                labyrinth[row, col - 1] = value.ToString();
+                _labyrinth[row, col - 1] = value.ToString();
             }
         }
 
         PrintLabyrinth();
     }
 
-    //Get matrix from the console
-    private static string[,] GetLabyrinth()
+    //Get matrix from the console and find starting position
+    private static void GetLabyrinthParams()
     {
-        
-        var dimension = int.Parse(Console.ReadLine());
-        var lab = new string[dimension, dimension];
-        for (int i = 0; i < dimension; i++)
+        var size = int.Parse(Console.ReadLine());
+        _labyrinth = new string[size, size];
+        for (int i = 0; i < size; i++)
         {
             var signs = Console.ReadLine();
-            for (int j = 0; j < dimension; j++)
+            for (int j = 0; j < size; j++)
             {
-                lab[i, j] = signs[j].ToString();
+                _labyrinth[i, j] = signs[j].ToString();
+                if (_labyrinth[i, j] == "*")
+                {
+                    startingPosition = new Cell(i, j, 0);
+                }
             }
         }
 
-        return lab;
+        if (startingPosition == null)
+        {
+            throw new AccessViolationException("Missing starting point!");
+        }
     }
 
     //Print result matrix
     private static void PrintLabyrinth()
     {
-        for (int i = 0; i < labyrinth.GetLongLength(0); i++)
+        for (int i = 0; i < _labyrinth.GetLongLength(0); i++)
         {
-            for (int j = 0; j < labyrinth.GetLongLength(1); j++)
+            for (int j = 0; j < _labyrinth.GetLongLength(1); j++)
             {
-                Console.Write(labyrinth[i, j] == "0" ? "u" : labyrinth[i, j]);
+                Console.Write(_labyrinth[i, j] == "0" ? "u" : _labyrinth[i, j]);
             }
 
             Console.WriteLine();
         }
-    }
-
-    //Check if cells next to the current is possible step,
-    //check if they are in the matrix and if their value is "0" (possible step)
-    private static bool HasStepTo(int row, int col)
-    {
-        return row >= 0 &&
-               row < labyrinth.GetLongLength(0) &&
-               col >= 0 &&
-               col < labyrinth.GetLongLength(1) &&
-               labyrinth[row, col] == "0";
-    }
-
-    //Traverse matrix for starting possition, marked with "*"
-    private static Cell GetStartingPosition()
-    {
-        for (int i = 0; i < labyrinth.GetLongLength(0); i++)
-        {
-            for (int j = 0; j < labyrinth.GetLongLength(1); j++)
-            {
-                if (labyrinth[i, j] == "*")
-                {
-                    return new Cell(i, j, 0);
-                }
-            }
-        }
-
-        throw new AccessViolationException("Missing starting point!");
     }
 }
