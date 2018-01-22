442b8caf262e5195c49c885c10b0e5c2ef3a37a6
LenHristova <len_5@abv.bg>
2018-01-22 20:36:29 +0200

    Add Tree and Binary Tree Implementation

---
+++ DataStructures/03. Data-Structures-Basic-Trees-Lab-Skeleton/Trees/Trees/BinaryTree.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class BinaryTree<T>
+{
+    public T Value { get; set; }
+
+    public BinaryTree<T> LeftChild { get; set; }
+
+    public BinaryTree<T> RightChild { get; set; }
+
+    public BinaryTree(T value, BinaryTree<T> leftChild = null, BinaryTree<T> rightChild = null)
+    {
+        this.Value = value;
+        this.LeftChild = leftChild;
+        this.RightChild = rightChild;
+    }
+
+    public void PrintIndentedPreOrder(int indent = 0)
+    {
+        Console.WriteLine(new string(' ', 2 * indent) + this.Value);
+
+        this.LeftChild?.PrintIndentedPreOrder(indent + 1);
+
+        this.RightChild?.PrintIndentedPreOrder(indent + 1);
+    }
+
+    public void EachInOrder(Action<T> action)
+    {
+        this.LeftChild?.EachInOrder(action);
+
+        action(this.Value);
+
+        this.RightChild?.EachInOrder(action);
+    }
+
+    public void EachPostOrder(Action<T> action)
+    {
+        this.LeftChild?.EachPostOrder(action);
+
+        this.RightChild?.EachPostOrder(action);
+
+        action(this.Value);
+    }
+}
