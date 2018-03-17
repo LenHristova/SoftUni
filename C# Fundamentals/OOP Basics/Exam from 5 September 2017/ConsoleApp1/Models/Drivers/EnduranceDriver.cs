d45bcc18d64bbb1adf9f526cba6097d27ae6fd33
LenHristova <len_5@abv.bg>
2018-03-17 13:21:57 +0200

    Remove ConsoleApp1.csproj

--- C# Fundamentals/OOP Basics/Exam from 5 September 2017/ConsoleApp1/Models/Drivers/EnduranceDriver.cs
+++
@@ -1,9 +0,0 @@
-﻿public class EnduranceDriver : Driver
-{
-    private const double FUEL_CONSUMPTION_PER_KM = 1.5;
-
-    public EnduranceDriver(string name, Car car) 
-        : base(name, car, FUEL_CONSUMPTION_PER_KM)
-    {
-    }
-}
--- C# Fundamentals/OOP Basics/Exam from 5 September 2017/ConsoleApp1/StartUp.cs
+++
@@ -1,9 +0,0 @@
-﻿public class StartUp
-{
-    static void Main()
-    {
-        var raceTower = new RaceTower();
-        var engine = new Engine(raceTower);
-        engine.Start();
-    }
-}
